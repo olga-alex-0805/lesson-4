821aa3e1f4637fbf485d6fd395de117a3ffea283
Olga Alex <olga.alex.0805@gmail.com>
2017-07-23 17:46:33 +0300

    Задание 4 Написать функцию getMax которая принимает на вход два аргумента в виде чисел. А возврыщает максимальное из этих двух. Так же, необходимо перегрузить эту функцию для работы с разными числовыми типами переменных (int, float)

---
+++ src/GetMax.java
@@ -0,0 +1,13 @@
+/**
+ * Created by AZ$E on 23.07.2017.
+ */
+//Задание 4 (инфо по функции GetMax не нашла, использовала Math.max)
+public class GetMax {
+    public static void main(String args[]){
+        System.out.println("Максимальные числа при сравнении двух аргументов:");
+        System.out.println(Math.max(6.564, 4.235));
+        System.out.println(Math.max(2.99, 2.07));
+        System.out.println(Math.max(-5.3976, -3.789));
+        System.out.println(Math.max(9.4546, -1.1));
+    }
+}
