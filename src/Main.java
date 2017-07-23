6e4781991438f5d6062b3832c14defa7ab58fe56
Olga Alex <olga.alex.0805@gmail.com>
2017-07-23 17:40:58 +0300

    ---Задание 1------------------------------------- Написать функцию которая считает в консоли до числа Х. Аргументы функции: число Х
    Например Х = 5.
    Вывод программы:
    1
    2
    3
    4
    5

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+/**
+ * Created by AZ$E on 23.07.2017.
+ *///задача1 счет до 5
+public class Main
+    {
+        public static void main(String[] args) {
+        int[] myList = new int[]{4, 2, 5, 1, 3, 8, 35, 87, 13};
+        int[] counter = new int[9];
+            int i = 1;
+            while (i < 6) {
+                System.out.print(i + " ");
+                i++;
+            }
+        }
+
+
+
+        }
