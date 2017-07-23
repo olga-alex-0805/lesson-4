abe6b5df5f3d4d4dd619ebc06ceb8c354c585f37
Olga Alex <olga.alex.0805@gmail.com>
2017-07-23 18:02:45 +0300

    вывод квадрата с помощью рекурсии

---
+++ src/Recursion2.java
@@ -0,0 +1,22 @@
+/**
+ * Created by AZ$E on 23.07.2017.
+ */
+public class Recursion2 {
+    public static void hello() {
+
+        System.out.println("+++");}
+            public static void main(String[]args){
+
+        hello();
+        hello();
+        hello();
+        }
+}
+
+
+
+
+
+
+
+
