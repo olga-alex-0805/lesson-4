71eee21789e1a395e78e0797bc1244290e040f29
Olga Alex <olga.alex.0805@gmail.com>
2017-07-23 17:53:49 +0300

    Задание 6 Решить задачу 2, без использования циклов. Использовав рекурсию.

---
+++ src/Method2.java
@@ -0,0 +1,13 @@
+/**
+ * Created by AZ$E on 23.07.2017.
+ */
+//задача 6, решена без использования циклов
+public class Method2 {
+
+        public static void hello(){
+            System.out.println("+++");
+            hello();
+            hello();
+        }
+    }
+
