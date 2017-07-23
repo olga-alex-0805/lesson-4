f0635a5cdef9e2066c3cabe18554b5353997cee9
Olga Alex <olga.alex.0805@gmail.com>
2017-07-23 18:04:56 +0300

    вывод прямоугольника с помощью цикла while

---
+++ src/Recursion1.java
@@ -0,0 +1,18 @@
+/**
+ * Created by AZ$E on 23.07.2017.
+ */
+public class Recursion1 {
+    public static void main(String[] args) throws Exception
+    {
+        int a = 0;
+        while (a<2)
+        {
+
+            System.out.println ("+++");
+            a++;
+
+        }
+
+    }
+
+    }
